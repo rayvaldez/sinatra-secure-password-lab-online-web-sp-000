dfe50d383790b8a0841e9132005087f9ecd5a97c
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-01-05 14:41:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
 
-    if user.username !=""
+    if user.username != nil
       redirect "/failure"
     else
       redirect "/failure"
